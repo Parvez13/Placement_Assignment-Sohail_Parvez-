cb59b59f0e0e6004e23437541eaea7a4588b8ca0
Parvez Sohail <66157611+Parvez13@users.noreply.github.com>
2023-06-14 14:47:24 +0530

    Update question_3.py

--- Pre_Placement/Assignment/Arrays_Lecture_1/question_3.py
+++ Pre_Placement/Assignment/Arrays_Lecture_1/question_3.py
@@ -1,10 +1,21 @@
 def find_index(nums, target):
-    for i in range(len(nums)):
-        if nums[i] == target:
-            return i 
-        elif nums[i] > target:
-            return i
-    return len(nums)
+    # for i in range(len(nums)):
+    #     if nums[i] == target:
+    #         return i 
+    #     elif nums[i] > target:
+    #         return i
+    # return len(nums)
+    start = 0
+    end = len(nums)-1
+    while(start<=end):
+        mid = (start+end)//2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return end + 1
 
 if __name__ == '__main__':
     nums = [1, 2,5,6]
