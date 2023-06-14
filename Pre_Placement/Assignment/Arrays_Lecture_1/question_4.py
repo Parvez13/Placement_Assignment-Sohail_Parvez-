530243cc820404599b5cd089039860b72a97b179
Parvez Sohail <66157611+Parvez13@users.noreply.github.com>
2023-06-14 15:01:22 +0530

    Update question_4.py

--- Pre_Placement/Assignment/Arrays_Lecture_1/question_4.py
+++ Pre_Placement/Assignment/Arrays_Lecture_1/question_4.py
@@ -1,8 +1,28 @@
-def return_largest(digits):
-        number = ''.join(str(x) for x in digits)
-        number = int(number)+ 1
-        number_list = [str(i) for i in str(number)]
-        return number_list
+def return_largest(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        digit_length = len(digits)
+        
+        i = digit_length - 1
+        
+        while digits[i] == 9 and i >= 0:
+            i -= 1
+        
+        if i == -1:
+            results = [0]*(digit_length + 1)
+            results[0] = 1
+            return results
+        
+        results = [0]*(digit_length)
+        
+        results[i] = digits[i] + 1
+        
+        for j in range(i-1, -1, -1):
+            results[j] = digits[j]
+        
+        return results
 
 if __name__ == '__main__':
     digits = [9,9,9,9,9]
