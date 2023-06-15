b0dfd6dd226eb7681fbc59d36f2617c167e0ab5d
Parvez Sohail <66157611+Parvez13@users.noreply.github.com>
2023-06-15 11:00:01 +0530

    Update question_5.py

--- Pre_Placement/Assignment/Arrays_Lecture_1/question_5.py
+++ Pre_Placement/Assignment/Arrays_Lecture_1/question_5.py
@@ -1,8 +1,16 @@
 def merge_arrays(nums1, nums2, m, n):
-    nums1 = list(filter(lambda x : x != 0, nums1))
-    nums2 = list(filter(lambda y: y !=0, nums2))
-    nums = nums1[:m] + nums2[:n]
-    return sorted(nums)
+    # Two pointer approach
+    i = m - 1
+    j = n - 1
+    k = m + n - 1
+    while j >= 0:
+        if i >= 0 and nums1[i] > nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
+        else:
+            nums1[k] = nums2[j]
+            j -= 1
+        k -= 1
 
 nums1 = [1, 2, 3, 0,0,0]
 nums2 = [2, 5, 6,1]
